764b5e2acbcce01eca4ea99a45142882656fd1e8
unknown <you@example.com>
2019-07-28 21:55:04 +0800

    spring

---
+++ demo/src/main/java/com/example/demo/until/LogEnum.java
@@ -0,0 +1,30 @@
+package com.example.demo.until;
+
+public enum LogEnum {
+
+    BUSSINESS("bussiness"),
+
+    PLATFORM("platform"),
+
+    DB("db"),
+
+    EXCEPTION("exception"),
+
+    ;
+
+
+    private String category;
+
+
+    LogEnum(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
